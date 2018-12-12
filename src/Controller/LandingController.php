983065f9a1566f03268bac3c3ed267e306b2d3c6
binyoum bayemi joseph <b.bj@hotmail.fr>
2018-12-12 09:21:00 +0100

    more advance

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -10,12 +10,29 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LandingController
 {
+    /**
+     * @Route("/")
+     */
+
+
     public function homepage()
     {
 
         return new Response('My first paid project and it is with Symfony GREAT!!!!!');
+    }
+
+    /**
+     * @Route("/{slug}")
+     */
+    public function show($slug)
+    {
+
+        return new Response(sprintf(
+            "HSE Global Security Assesment: %s", $slug
+        ));
     }
 }
